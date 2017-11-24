7f2f71ebb36c927af69ba978afdaa28853206d29
DanielCornejoITM <danielcornejo1994@gmail.com>
2017-11-24 11:51:59 -0600

    No se que hice

--- app/Sesion/_registrar.php
+++ app/Sesion/_registrar.php
@@ -2,10 +2,12 @@
 
 session_start(); # importante iniciar session al comienzo de la página
  
-
+#Cifradas
 $Nombre = $_POST['nom'];
 $Apellido = $_POST['ape'];
-$Correo = $_POST['mail'];
+$Correo = md5($_POST['mail']);
+$Contra = md5($_POST['mot']);
+#No cifradas
 $Calle = $_POST['rue'];
 $Numero = $_POST['numb'];
 $Colonia = $_POST['neight'];
@@ -13,7 +15,6 @@ $CP = $_POST['cp'];
 $Ciudad = $_POST['ville'];
 $Estado = $_POST['etat'];
 $Pais = $_POST['pay'];
-$Contra = $_POST['mot'];
 
 
 if(!$_POST['captcha'] == $_SESSION['codigo']){
@@ -21,10 +22,14 @@ header("Location: index.php");
 exit;  
 }
 
+include "../Conexion.php";
+
+
 
 echo "Nombre: ".$Nombre;
 echo "<br>Apellido: ".$Apellido;
 echo "<br>Correo: ".$Correo;
+echo "<br>Contraseña: ".$Contra;
 echo "<br>Calle: ".$Calle;
 echo "<br>Numero: ".$Numero;
 echo "<br>Colonia: ".$Colonia;
@@ -32,7 +37,7 @@ echo "<br>CP: ".$CP;
 echo "<br>Ciudad: ".$Ciudad;
 echo "<br>Estado: ".$Estado;
 echo "<br>Pais: ".$Pais;
-echo "<br>Contraseña: ".$Contra;
+
 
 
 ?>
